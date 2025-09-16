2dee27dcea7c3c2a7f0f6f1d297132c17ff2dd5e
Jacob Flynn <novastreakgaming@outlook.com>
2025-09-16 12:23:54 -0400

    Made required changes to TextSizeAdapter and MainActivity

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -4,22 +4,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val c: Context, private val items: Array<Int>) : BaseAdapter() {
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
+    override fun getCount() = items.size
+
+    override fun getItem(position: Int) = items[position]
+
+    override fun getItemId(position: Int) = position.toLong()
     }
 
-    override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
-    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?) =
+        if (convertView != null) {
+            convertView as TextView
+        } else{
+            TextView(c)
+        }.apply{
+            text = items[position].toString()
+            textSize = 22f
+        }
 
-    override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return getView(position, convertView, parent).apply{
+            textSize = items[position].toFloat()
     }
 
 }
